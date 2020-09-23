dd485263d80e1bf7ec1bb9efdd1b65344711cc2b
Patrick Meng <patm314@gmail.com>
2020-09-23 13:44:09 -0500

    added bit of code to login

--- app/src/main/java/com/wwave/wave/LogIn.java
+++ app/src/main/java/com/wwave/wave/LogIn.java
@@ -2,7 +2,9 @@ package com.wwave.wave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class LogIn extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //Allow users to store their own data
+        SharedPreferences userInformation = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String password = userInformation.getString("agreedToTerms","this username does not exist1223323723465217513645346714");
+
     }
 }
