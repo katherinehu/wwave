953965e49a85fd6403084d438cc80416973415d4
Patrick Meng <patm314@gmail.com>
2020-09-23 20:54:39 -0500

    activity navigation

--- app/src/main/java/com/wwave/wave/CreateAccount.java
+++ app/src/main/java/com/wwave/wave/CreateAccount.java
@@ -2,13 +2,56 @@ package com.wwave.wave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateAccount extends AppCompatActivity {
 
+    EditText et_name;
+    EditText et_createdUsername;
+    EditText et_createdPassword;
+    EditText et_reenteredCreatedPassword;
+    Button btn_signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        et_name = findViewById(R.id.et_name);
+        et_createdPassword = findViewById(R.id.et_createdPassword);
+        et_reenteredCreatedPassword = findViewById(R.id.et_reenteredCreatedPassword);
+        et_createdUsername = findViewById(R.id.et_createdUsername);
+        btn_signUp = findViewById(R.id.btn_signUp);
+
+        final SharedPreferences userInformation = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        btn_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = et_name.getText().toString();
+                String password1 = et_createdPassword.getText().toString();
+                String password2 = et_reenteredCreatedPassword.getText().toString();
+                String username = et_createdUsername.getText().toString();
+                if (userInformation.getString(username,"not found123").equals("not found123")) {
+                    if (password1.equals(password2)) {
+                        //We create the account
+                        SharedPreferences.Editor editor = userInformation.edit();
+                        editor.putString(username,password1);
+                        editor.putString(username+"NAME",name);
+                        editor.commit();
+                    } else {
+                        Toast youBlind = Toast.makeText(getApplicationContext(),"Passwords don't match",Toast.LENGTH_SHORT);
+                        youBlind.show();
+                    }
+                } else {
+                    Toast found = Toast.makeText(getApplicationContext(),"Username taken, make another",Toast.LENGTH_SHORT);
+                    found.show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/wwave/wave/Welcome.java
+++ app/src/main/java/com/wwave/wave/Welcome.java
@@ -2,13 +2,23 @@ package com.wwave.wave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
+
+    TextView tv_nameOfPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        tv_nameOfPerson = findViewById(R.id.tv_nameOfPerson);
+
+        Intent info = getIntent();
+        String name = info.getStringExtra("name");
+        tv_nameOfPerson.setText(name);
     }
 }
