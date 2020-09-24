c663f5d7a20b80744a1eb9666a85fda069e1c36f
katherine hu <hu.z.katherine@gmail.com>
2020-09-23 22:02:43 -0400

    open on login

--- app/src/main/java/com/wwave/wave/MainActivity.java
+++ app/src/main/java/com/wwave/wave/MainActivity.java
@@ -2,6 +2,7 @@ package com.wwave.wave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int eee = 6;
+
+        Intent loginPage = new Intent(this, LogIn.class);
+        startActivity(loginPage);
     }
 }
