e2ac2b4674c4169055a5d848129f401fe75b826e
katherine hu <hu.z.katherine@gmail.com>
2020-09-30 22:11:33 -0400

    go To website

--- app/src/main/java/com/wwave/wave/About.java
+++ app/src/main/java/com/wwave/wave/About.java
@@ -3,13 +3,17 @@ package com.wwave.wave;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
     Button btnSendEmail;
+    Button btnGoToWebsite;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,25 @@ public class About extends AppCompatActivity {
             }
         });
 
+        btnGoToWebsite = findViewById(R.id.btnGoToWebsite);
+        btnGoToWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://docs.google.com/document/d/1VTDw-DsQqZ8eiyUjsgbyr1UpM_ZmJNQ0QWuHrSHsOno/edit";
+                Uri webpage = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                } else {
+                    Log.d("ImplicitIntents", "URL can’t be handled");
+                }
+            }
+        });
     }
+
+
+
+
+
 }
