e97c0252c2e8e318ca8b9a6c64ad947790a0459f
Patrick Meng <patm314@gmail.com>
2020-11-25 23:41:43 -0500

    working

--- app/src/main/java/com/wwave/wave/Homee.java
+++ app/src/main/java/com/wwave/wave/Homee.java
@@ -4,25 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.UnsupportedMimeTypeException;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import com.jjoe64.graphview.Viewport;
-
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
-public class Homee extends AppCompatActivity implements View.OnClickListener{
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Homee extends AppCompatActivity implements View.OnClickListener {
 
     private CardView rednessCV, accelerationCV, aboutCV, feedbackCV;
     private TextView uvIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,8 +42,13 @@ public class Homee extends AppCompatActivity implements View.OnClickListener{
 
         uvIndex = findViewById(R.id.uvIndex);
 
-        this.getUV();
-
+        try {
+            String result = new getUV().execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -52,26 +56,54 @@ public class Homee extends AppCompatActivity implements View.OnClickListener{
         Intent i;
 
         switch (v.getId()) {
-            case R.id.rednessCV: i =  new Intent(this, CustomCamera.class);startActivity(i);break ;
-            case R.id.accelerationCV: i =  new Intent(this, AccelActivity.class);startActivity(i);break ;
-            case R.id.aboutCV: i =  new Intent(this, About.class);startActivity(i);break ;
-            case R.id.feedbackCV: i =  new Intent(this, EmailActivity.class);startActivity(i);break ;
-            default: break ;
+            case R.id.rednessCV:
+                i = new Intent(this, CustomCamera.class);
+                startActivity(i);
+                break;
+            case R.id.accelerationCV:
+                i = new Intent(this, AccelActivity.class);
+                startActivity(i);
+                break;
+            case R.id.aboutCV:
+                i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+            case R.id.feedbackCV:
+                i = new Intent(this, EmailActivity.class);
+                startActivity(i);
+                break;
+            default:
+                break;
         }
 
     }
 
-    public void getUV() {
+    public class getUV extends AsyncTask<String,String,String> {
 
-        OkHttpClient client = new OkHttpClient();
+        @Override
+        protected String doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url("https://api.openuv.io/api/v1/uv?lat=-33.34&lng=115.342&dt=2018-01-24T10%3A50%3A52.283Z")
-                .get()
-                .addHeader("x-access-token", "2d68077ff5a271166cf9979e619ae1f1")
-                .build();
+            Request request = new Request.Builder()
+                    .url("https://api.openuv.io/api/v1/uv?lat=-33.34&lng=115.342&dt=2018-01-24T10%3A50%3A52.283Z")
+                    .get()
+                    .addHeader("x-access-token", "2d68077ff5a271166cf9979e619ae1f1")
+                    .build();
 
-        Response response = client.newCall(request).execute();
+
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Headers headers = response.headers();
+            String stringResponse = response.body().toString();
+            int e = 6;
+            return null;
+        }
+    }
+}
 
 //        Document doc;
 //
@@ -83,7 +115,3 @@ public class Homee extends AppCompatActivity implements View.OnClickListener{
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-    }
-
-
-}
