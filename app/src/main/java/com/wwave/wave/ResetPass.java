0dc16c9c08f309885959a1d9ecdec6a7406e20c8
katherine hu <hu.z.katherine@gmail.com>
2020-12-01 21:25:39 -0500

    made buttons smaller

--- app/src/main/java/com/wwave/wave/ResetPass.java
+++ app/src/main/java/com/wwave/wave/ResetPass.java
@@ -12,6 +12,7 @@ public class ResetPass extends AppCompatActivity {
 
     EditText et_pass1;
     EditText et_pass2;
+    EditText et_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +22,21 @@ public class ResetPass extends AppCompatActivity {
         //get from ui
         et_pass1 = findViewById(R.id.et_pass1);
         et_pass2 = findViewById(R.id.et_pass2);
+        et_user = findViewById(R.id.et_user);
+
 
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor edit = pref.edit();
 
-        edit.remove()
+        edit.remove(et_user.toString());
+        edit.putString(et_user,et_pass1);
 
+        edit.putString(username+"NAME",name);
+        edit.putString("nameOfUser",name);
+
+
+        edit.apply();
 
 
     }
